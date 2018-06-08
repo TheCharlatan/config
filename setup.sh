63ce2a7f12989fe5b8fb783a84b6b942a4a9455d
TheCharlatan <seb.kung@gmail.com>
2018-06-08 15:32:02 +0200

    Add powerlevel9k

--- setup.sh
+++ setup.sh
@@ -1,2 +1,7 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 chsh -s $(which zsh)
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+sed -i 's/ZSH-THEME="robbyrussel"/ZSH_THEME="powerlevel9k/powerlevel9k"/g' ~/.zshrc
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+sed -i '/plugins=(/a \ \ zsh-autosuggestions' ~/.zshrc
+sed -i '/plugins=(rails/ { N; d; }' ~/.zshrc
