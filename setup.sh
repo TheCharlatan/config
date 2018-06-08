429fc3db1c383ff42a894d9e0b12eec7292dce63
TheCharlatan <seb.kung@gmail.com>
2018-06-08 15:56:34 +0200

    Add vundle and font package

--- setup.sh
+++ setup.sh
@@ -5,3 +5,7 @@ sed -i 's/ZSH-THEME="robbyrussel"/ZSH_THEME="powerlevel9k/powerlevel9k"/g' ~/.zs
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 sed -i '/plugins=(/a \ \ zsh-autosuggestions' ~/.zshrc
 sed -i '/plugins=(rails/ { N; d; }' ~/.zshrc
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+cp vimrc ~/.vimrc
+git clone https://github.com/flazz/vim-colorschemes.git ~/.vim/bundle/colorschemes
+vim -c ":PluginInstall"
